fd21bd6f2791c75b64744e964b0d44a098270f4d
chriscaira <chriscaira@nousot.com>
2025-01-01 12:02:10 -0600

    small fix

--- src/components/PDFViewer/PDFViewer.js
+++ src/components/PDFViewer/PDFViewer.js
@@ -14,11 +14,13 @@ const PDFViewer = ({ pdfUrl = '/resume.png' }) => {
 
     return (
 
-        <div className='btn btn-outline'>
-            <button onClick={handleButtonClick}>View Resume</button>
+        <div className='link link--icon'>
+            <button onClick={handleButtonClick}>
+                View Resume
+            </button>
             {showPDF && (
                 <div className="pdf-viewer">
-                    <img src='/resume.png' alt='resume' style={{ maxWidth: '100vh', maxHeight: '90vh', objectFit: 'contain' }}></img>
+                    <img src={pdfUrl} alt='resume' style={{ maxWidth: '100vh', maxHeight: '90vh', objectFit: 'contain' }}></img>
                     {/* <object data={pdfUrl} title="PDF Viewer" type='image/png'></object> */}
                     <button className="close-button" onClick={handleCloseClick}>Close</button>
                 </div>
